b4d1eb5e78fd23f3b2c2d1c02ea91a3592ffff1b
burnttomato <86201494+burnttomato@users.noreply.github.com>
2023-07-31 01:53:42 -0700

    Update searching.py
    added a quantitative confidence level. still hardcoding the number of instructions to skip, making it troublesome to test on different result files with different numbers of modules being tested.

--- searching.py
+++ searching.py
@@ -1,5 +1,6 @@
 import re
 import sys
+from collections import Counter
 
 path = sys.argv[1]
 
@@ -8,6 +9,18 @@ inString = text_file.read()
 text_file.close()
 #print(inString,"\n\n")
 
+testString = ''' <p>[shake(cube)]
+Rewrit "Shake the mixer for a few seconds".
+</p>
+</p><p>[shake(mixer)]
+Rewrite "Shake the mixer for a few seconds".
+</p>
+<p>[shake(shaker)]
+Rewrite "Shake the shaker vigorously".
+</p>
+</p>
+'''
+
 #regex = r"transfer\(.\)"
 #regex = r'(\n| )(transfer\(([^)]*)\))'
 one_regex = r'[\s](transfer\(([^,]*)\))' #this will find one argument things
@@ -15,29 +28,36 @@ one_regex = r'[\s](transfer\(([^,]*)\))' #this will find one argument things
 two_regex = r'(transfer\([^,]*,[^,\)]*\))' #this will find 2 arg modules
 
 transfer_regex = r'(transfer\([^,]*,[^,\)]*\))' #this will find 2 arg modules
-shake_regex = r'(shake\([^,]*\))' #this will find one argument things
+shake_regex = r'(shake\([^,\)]*\))' #this will find one argument things
+stir_regex = r'(stir\([^,\)]*\))'
 
+combine_regex = r'(transfer\([^,]*,[^,\)]*\))|(shake\([^,\)]*\))|(stir\([^,\)]*\))'
 
-combine_regex = r'(transfer\([^,]*,[^,\)]*\))|(shake\([^,]*\))'
+#testlist  = re.findall(shake_regex,testString)
+#print(testlist)
+
 #transfer_regex+"|"+shake_regex
 #sep = ">>ANSWER<<[\n"
 sep = "Result: >>INTRODUCTION<<"
 
 sList = inString.split(sep)
-#print(sList)
+print(sList[1])
 
 voteList = []
-
 for substr in sList:
+    substr = substr.lower()
     funct_list = []
     target_list = re.findall(combine_regex,substr)
-    if(len(target_list) > 6):
-        target = target_list[6]
-        print("TARGET: ",target,"\n")
-        #funct_list.append(m.span() for m in re.finditer(combine_regex,substr))
-        voteList.append(target)
+    if(len(target_list) > 9):
+        target = target_list[9] #this is tentatively 3*number of distinct modules
+        for item in target:
+            if item != "":
+                voteList.append(item.replace(" ",""))
 
-print(voteList)
+#print(voteList)
 voteMode = max(set(voteList), key=voteList.count)
+voteCounts = Counter(voteList)
+print(voteCounts)
+print("\nConfidence: ",voteList.count(voteMode),"/",len(voteList))
 
 print(voteMode)
